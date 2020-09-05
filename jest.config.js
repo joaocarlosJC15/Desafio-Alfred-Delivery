f2e711d2035e381b06fc36c8251c2318d566eb3e
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-05 09:47:58 -0300

    chore: adicionado a biblioteca 'jest' para testes unitarios e de integracao e as configuracoes para uso

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
