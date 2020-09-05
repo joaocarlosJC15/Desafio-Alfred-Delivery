308ff9a24f4cd1ec74a240f30b2fd8b389c794f3
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-05 15:57:29 -0300

    feat: implementada a validacao 'DateFieldsValidation'

---
+++ src/validation/general/date-field/date-field-validation.ts
@@ -0,0 +1,19 @@
+import { Validation } from '@/presentation/protocols'
+import { InvalidParamError } from '@/errors'
+
+export class DateFieldValidation implements Validation {
+  constructor (private readonly fieldName: string) {
+    this.fieldName = fieldName
+  }
+
+  validate (input: any): Error {
+    if (input[this.fieldName]) {
+      const date = new Date(input[this.fieldName])
+
+      if (date.toString() !== 'Invalid Date') {
+        return
+      }
+    }
+    return new InvalidParamError(this.fieldName)
+  }
+}
