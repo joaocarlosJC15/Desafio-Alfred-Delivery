8c69d443efe642c86b694f45245f0cc9d71857cf
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-05 17:18:56 -0300

    feat: implementada as interfaces 'CreateUserRepository', 'HashComparer' e 'Hasher'

---
+++ src/domain/protocols/db/user/create-user-repository.ts
@@ -0,0 +1,6 @@
+import { CreateUserModel } from '../../../../domain/usecases/user/create/create-user'
+import { UserModel } from '../../../../domain/models/user'
+
+export interface CreateUserRepository {
+  create: (user: CreateUserModel) => Promise<UserModel>
+}
