80ebcea73774f210bb90caa76616d3d8c18584b7
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-05 16:08:52 -0300

    test: garantir que o CreateUserController retorne o statusCode caso ocorra algum erro nas validacoes

--- src/presentation/controllers/user/create/create-user-controller.spec.ts
+++ src/presentation/controllers/user/create/create-user-controller.spec.ts
@@ -1,11 +1,14 @@
-import { HttpRequest } from '@/presentation/protocols'
+import { HttpRequest, Validation } from '@/presentation/protocols'
 import { CreateUserControler } from './create-user-controller'
 import { CreateUser, CreateUserModel } from '@/domain/usecases/user/create-user'
 import { UserModel } from '@/domain/models/user'
+import { MissingParamError, InvalidParamError } from '@/errors'
+import { badRequest } from '@/presentation/http/responses'
 
 interface SutTypes {
   sut: CreateUserControler
   createUserStub: CreateUser
+  validationStub: Validation
 }
 
 const makeFakeDate = new Date()
@@ -38,18 +41,60 @@ const makeCreateUser = (): CreateUser => {
   return new CreateUserStub()
 }
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+
+  return new ValidationStub()
+}
+
 const makeSut = (): SutTypes => {
   const createUserStub = makeCreateUser()
-  const sut = new CreateUserControler(createUserStub)
+  const validationStub = makeValidation()
+  const sut = new CreateUserControler(createUserStub, validationStub)
 
   return {
     sut,
-    createUserStub
+    createUserStub,
+    validationStub
   }
 }
 
 describe('CreateUserController', () => {
-  test('CreateUser.create deve ser chamado com os valores corretos', async () => {
+  test('CreateUserController deve retornar 400 se alguma validação retornar o erro "MissingParamError"', async () => {
+    const { sut, validationStub } = makeSut()
+
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('field')))
+  })
+
+  test('CreateUserController deve retornar 400 se alguma validação retornar o erro "InvalidParamError"', async () => {
+    const { sut, validationStub } = makeSut()
+
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new InvalidParamError('field'))
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('field')))
+  })
+
+  test('CreateUserController deve retornar 400 se alguma validação retornar o erro "InvalidParamError"', async () => {
+    const { sut, validationStub } = makeSut()
+
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new InvalidParamError('field'))
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('field')))
+  })
+
+  test('CreateUserController.createUser.create deve ser chamado com os valores corretos', async () => {
     const { sut, createUserStub } = makeSut()
 
     const createUserSpy = jest.spyOn(createUserStub, 'create')
