8d4d508822f3438af3b4081b9a4db161ed1c8c2b
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-05 22:46:15 -0300

    chore: implementada a migration 'create_users_table'

---
+++ src/infra/db/postgresql/config/migrations/20200905213826_create_users_table.ts
@@ -0,0 +1,15 @@
+import Knex from 'knex'
+
+export async function up (knex: Knex): Promise<any> {
+  return knex.schema.createTable('users', (table: Knex.TableBuilder) => {
+    table.increments('id').primary()
+    table.string('name', 200).notNullable()
+    table.string('email', 200).notNullable()
+    table.string('password', 200).notNullable()
+    table.date('birthDate').notNullable()
+  })
+}
+
+exports.down = function (knex: Knex) {
+  return knex.schema.dropTable('users')
+}
