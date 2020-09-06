5fa515009e4911d224646ce44298c5b625cdd7f7
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-06 20:19:24 -0300

    refactor: acrescentada a validacao 'DateFieldValidation' para o campo 'birthDate' para o CreateUserController

--- src/main/factories/controllers/user/create-user/create-user-controller-validation-factory.ts
+++ src/main/factories/controllers/user/create-user/create-user-controller-validation-factory.ts
@@ -1,14 +1,15 @@
 import { Validation } from '@/presentation/protocols/validation'
 
-import { RequiredFieldValidation, ValidationComposite, CompareFieldsValidation } from '@/validation'
+import { RequiredFieldValidation, ValidationComposite, CompareFieldsValidation, DateFieldValidation } from '@/validation'
 
 export const makeCreateUserControllerValidation = (): ValidationComposite => {
   const validations: Validation[] = []
 
-  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+  for (const field of ['name', 'email', 'birthDate', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field))
   }
   validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+  validations.push(new DateFieldValidation('birthDate'))
 
   return new ValidationComposite(validations)
 }
