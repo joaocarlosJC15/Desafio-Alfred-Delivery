c83d3bc1444f0080bcf599b58a37baf992511080
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-06 10:12:49 -0300

    feat: implementado o Middleware 'cors' para especificar a configuracao do CORS

---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const cors = (request: Request, response: Response, next: NextFunction): void => {
+  response.set('Access-Control-Allow-Origin', '*')
+  response.set('Access-Control-Allow-Methods', '*')
+  response.set('Access-Control-Allow-Headers', '*')
+  next()
+}
