d21d947148935f4eea34a20d07f496bb175474f7
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-06 08:03:51 -0300

    feat: garantir que 'UserRepository.getByEmail' retorne um usuario se a acao for bem sucedida

--- src/infra/db/postgresql/user/user-repository.ts
+++ src/infra/db/postgresql/user/user-repository.ts
@@ -3,15 +3,32 @@ import { UserModel } from '@/domain/models/user'
 import { CreateUserModel } from '@/domain/usecases/user/create/create-user'
 import connection from '../config/connection'
 import { serializeToUser } from './user-serialize'
+import { GetUserByEmailRepository } from '@/domain/protocols/db/user/get-user-by-email-repository'
 
-export class UserRepository implements CreateUserRepository {
+export class UserRepository implements CreateUserRepository, GetUserByEmailRepository {
   tableName = 'users'
 
   async create (userCreate: CreateUserModel): Promise<UserModel> {
     const data = await connection(this.tableName).insert(userCreate)
 
-    const userData = await connection.select().from(this.tableName).where('users.id', data[0])
+    if (data) {
+      const userData = await connection.select().from(this.tableName).where('users.id', data[0])
 
-    return serializeToUser(userData[0])
+      if (userData) {
+        return serializeToUser(userData[0])
+      }
+    }
+
+    return null
+  }
+
+  async getByEmail (email: string): Promise<UserModel> {
+    const data = await connection.select().from(this.tableName).where('users.email', email)
+
+    if (data) {
+      return serializeToUser(data[0])
+    }
+
+    return null
   }
 }
