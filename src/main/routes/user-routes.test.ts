d257bb81024f7c2913359af9210e6bb01da01492
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-06 21:20:32 -0300

    test: implementados os testes de integracao para a rota POST:/login

--- src/main/routes/user-routes.test.ts
+++ src/main/routes/user-routes.test.ts
@@ -1,7 +1,9 @@
 import request from 'supertest'
+import { hash } from 'bcrypt'
 
 import app from '../config/app'
 import connection from '@/infra/db/postgresql/config/connection'
+import { CreateUserModel } from '@/domain/usecases/user/create/protocols/create-user'
 
 const tableName = 'users'
 const fakeDate = new Date()
@@ -12,6 +14,13 @@ const makeFakeRequest = (): any => ({
   birthDate: fakeDate,
   password: 'any_password',
   passwordConfirmation: 'any_password'
+})
+
+const makeFakeCreateUser = (): CreateUserModel => ({
+  name: 'name',
+  email: 'email@mail.com',
+  birthDate: fakeDate,
+  password: 'password'
 })
 
 describe('User Routes', () => {
@@ -33,6 +42,52 @@ describe('User Routes', () => {
         .post('/users')
         .send(makeFakeRequest())
         .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('/login deve retornar 200 para o caso de sucesso', async () => {
+      const password = await hash(makeFakeCreateUser().password, 12)
+
+      const fakeUser = Object.assign({}, makeFakeCreateUser())
+      fakeUser.password = password
+
+      await connection(tableName).insert(fakeUser)
+
+      await request(app)
+        .post('/login')
+        .send({
+          email: makeFakeCreateUser().email,
+          password: makeFakeCreateUser().password
+        })
+        .expect(200)
+    })
+
+    test('/login deve retornar 404 usuário não encontrado', async () => {
+      await request(app)
+        .post('/login')
+        .send({
+          email: makeFakeCreateUser().email,
+          password: makeFakeCreateUser().password
+        })
+        .expect(404)
+    })
+
+    test('/login deve retornar 401 para senha invalida', async () => {
+      const password = await hash(makeFakeCreateUser().password, 12)
+
+      const fakeUser = Object.assign({}, makeFakeCreateUser())
+      fakeUser.password = password
+
+      await connection(tableName).insert(fakeUser)
+
+      await request(app)
+        .post('/login')
+        .send({
+          email: makeFakeCreateUser().email,
+          password: 'wrong_password'
+        })
+        .expect(401)
     })
   })
 })
