ec1414053182644613f3869a67a6ab087caef469
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-07 13:09:37 -0300

    refactor: renomeado diretorios

--- src/main/factories/middlewares/authentication-middleware-factory.ts
+++ src/main/factories/middlewares/authentication-middleware-factory.ts
@@ -1,6 +1,6 @@
 import { Middleware } from '@/presentation/protocols'
 import { AuthenticationMiddleware } from '@/presentation/middlewares/authentication-middleware'
-import { makeGetUserByTokenUsecase } from '../usecases/user/get-user-by-token/get-user-by-token-usecase-factory'
+import { makeGetUserByTokenUsecase } from '../usecases/user/get-by-token/get-user-by-token-usecase-factory'
 
 export const makeAuthenticationMiddleware = (): Middleware => {
   return new AuthenticationMiddleware(makeGetUserByTokenUsecase())
