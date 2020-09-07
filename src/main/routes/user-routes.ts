892cc4b822aa44a56280d689108cce6befbf0062
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-07 13:12:34 -0300

    refactor: renomeado diretorio

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
-import { makeCreateUserController } from '../factories/controllers/user/create-user/create-user-controller-factory'
+import { makeCreateUserController } from '../factories/controllers/user/create/create-user-controller-factory'
 import { makeAuthenticationUserController } from '../factories/controllers/user/authentication/authentication-user-controller-factory'
 
 export default (router: Router): void => {
