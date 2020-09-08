9402cf373be89f6b7137cb13045cf1d58a4a9160
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-08 17:58:51 -0300

    feat: implementada a rota GET '/categories/:category_id'

--- src/main/routes/category-routes.ts
+++ src/main/routes/category-routes.ts
@@ -4,9 +4,11 @@ import { makeAuthenticationMiddleware } from '../factories/middlewares/authentic
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeCreateCategoryController } from '../factories/controllers/category/create/create-category-controller-factory'
 import { makeGetCategoriesByUserController } from '../factories/controllers/category/get/get-categories-by-user-controller-factory'
+import { makeGetCategoryByUserController } from '../factories/controllers/category/get/get-category-by-user-controller-factory'
 
 export default (router: Router): void => {
   const authMiddleware = adaptMiddleware(makeAuthenticationMiddleware())
   router.post('/categories', authMiddleware, adaptRoute(makeCreateCategoryController()))
   router.get('/categories', authMiddleware, adaptRoute(makeGetCategoriesByUserController()))
+  router.get('/categories/:category_id', authMiddleware, adaptRoute(makeGetCategoryByUserController()))
 }
