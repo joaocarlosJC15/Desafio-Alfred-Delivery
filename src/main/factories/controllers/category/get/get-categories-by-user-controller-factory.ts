562a35319d3a95f357c6da11b15ad2c52da91464
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-08 17:13:13 -0300

    refactor: renomeado diretório

--- src/main/factories/controllers/category/get/get-categories-by-user-controller-factory.ts
+++ src/main/factories/controllers/category/get/get-categories-by-user-controller-factory.ts
@@ -1,6 +1,6 @@
 import { Controller } from '@/presentation/protocols'
 import { GetCategoriesByUserController } from '@/presentation/controllers/category/get/get-all-by-user/get-categories-by-user-controller'
-import { makeCategoryRepository } from '@/main/factories/repositores/category/category-repository-factory'
+import { makeCategoryRepository } from '@/main/factories/repositories/category/category-repository-factory'
 
 export const makeGetCategoriesByUserController = (): Controller => {
   const controller = new GetCategoriesByUserController(makeCategoryRepository())
--- src/main/factories/usecases/category/get/get-categories-by-user-usecase-factory.ts
+++ src/main/factories/usecases/category/get/get-categories-by-user-usecase-factory.ts
@@ -1,5 +1,5 @@
 import { GetCategoriesByUserUseCase } from '@/domain/usecases/category/get/get-all-by-user/get-categories-by-user-usecase'
-import { makeCategoryRepository } from '@/main/factories/repositores/category/category-repository-factory'
+import { makeCategoryRepository } from '@/main/factories/repositories/category/category-repository-factory'
 
 export const makeGetCategoriesByUserUseCase = (): GetCategoriesByUserUseCase => {
   const getCategoriesByUserUseCase = new GetCategoriesByUserUseCase(makeCategoryRepository())
