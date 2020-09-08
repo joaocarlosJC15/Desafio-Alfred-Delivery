7eee40472f66371185bc9dfd0b099b243898e267
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-08 16:04:04 -0300

    refactor: corrigido o nome do factory para o usecase 'CreateCategoryUsecase'

--- src/main/factories/controllers/category/create/create-category-controller-factory.ts
+++ src/main/factories/controllers/category/create/create-category-controller-factory.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@/presentation/protocols'
 import { makeCreateCategoryControllerValidation } from './create-category-controller-validation-factory'
 import { CreateCategoryControler } from '@/presentation/controllers/category/create/create-category-controller'
-import { makeCreateCategoryUseCase } from '@/main/factories/usecases/category/create/create-user-usecase-factory'
+import { makeCreateCategoryUseCase } from '@/main/factories/usecases/category/create/create-category-usecase-factory'
 
 export const makeCreateCategoryController = (): Controller => {
   const controller = new CreateCategoryControler(
