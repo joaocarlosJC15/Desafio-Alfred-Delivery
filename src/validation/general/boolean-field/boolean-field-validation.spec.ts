04df22555046774404eb24d8bd551a7d2a6ba301
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-09 16:57:54 -0300

    test: implementados os testes necessarios para  a validacao 'BooleanFieldValidation'

---
+++ src/validation/general/boolean-field/boolean-field-validation.spec.ts
@@ -0,0 +1,44 @@
+import { BooleanFieldValidation } from './boolean-field-validation'
+import { InvalidParamError } from '@/errors'
+
+const makeSut = (): BooleanFieldValidation => {
+  return new BooleanFieldValidation('field')
+}
+
+describe('BooleanFieldValidation', () => {
+  test('RequiredFieldValidation.validate não deve retornar nada se a propriedade fornecida for true', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: true })
+    expect(error).toBeUndefined()
+  })
+
+  test('RequiredFieldValidation.validate não deve retornar nada se a propriedade fornecida for false', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: false })
+    expect(error).toBeUndefined()
+  })
+
+  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se a propriedade a ser testada for uma string', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'number' })
+    expect(error).toEqual(new InvalidParamError('field'))
+  })
+
+  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se a propriedade a ser testada for um numero', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 1 })
+    expect(error).toEqual(new InvalidParamError('field'))
+  })
+
+  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se a propriedade a ser testada for um objeto', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: {} })
+    expect(error).toEqual(new InvalidParamError('field'))
+  })
+
+  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se a propriedade a ser testada for um undefined', () => {
+    const sut = makeSut()
+    const error = sut.validate({})
+    expect(error).toEqual(new InvalidParamError('field'))
+  })
+})
