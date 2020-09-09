7b6f15e708033c81115a8ad8ddc3f46926f3c7e0
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-09 16:56:58 -0300

    refactor: corrigido os nomes das mensagens para os testes da validacao 'IntegerFieldValidation'

--- src/validation/general/integer-field/integer-field-validation.spec.ts
+++ src/validation/general/integer-field/integer-field-validation.spec.ts
@@ -12,31 +12,31 @@ describe('IntegerFieldValidation', () => {
     expect(error).toBeUndefined()
   })
 
-  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se for a propriedade a ser testada for uma string', () => {
+  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se a propriedade a ser testada for uma string', () => {
     const sut = makeSut()
     const error = sut.validate({ field: 'number' })
     expect(error).toEqual(new InvalidParamError('field'))
   })
 
-  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se for a propriedade a ser testada for um numero real', () => {
+  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se a propriedade a ser testada for um numero real', () => {
     const sut = makeSut()
     const error = sut.validate({ field: 1.2 })
     expect(error).toEqual(new InvalidParamError('field'))
   })
 
-  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se for a propriedade a ser testada for um objeto', () => {
+  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se a propriedade a ser testada for um objeto', () => {
     const sut = makeSut()
     const error = sut.validate({ field: {} })
     expect(error).toEqual(new InvalidParamError('field'))
   })
 
-  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se for a propriedade a ser testada for um booleano', () => {
+  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se a propriedade a ser testada for um booleano', () => {
     const sut = makeSut()
     const error = sut.validate({ field: true })
     expect(error).toEqual(new InvalidParamError('field'))
   })
 
-  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se for a propriedade a ser testada for um undefined', () => {
+  test('IntegerFieldValidation.validate deve retonar o erro "InvalidParamError" se a propriedade a ser testada for um undefined', () => {
     const sut = makeSut()
     const error = sut.validate({})
     expect(error).toEqual(new InvalidParamError('field'))
