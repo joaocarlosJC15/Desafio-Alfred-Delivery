baceaa3551c92df4ba4f3d08dad6868ce9770d46
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-09 17:03:51 -0300

    refactor: acrescentada validacao para verificar a quantidade de digitos de um numero inteiro para a valicao 'IntegerFieldValidation'

--- src/validation/general/integer-field/integer-field-validation.ts
+++ src/validation/general/integer-field/integer-field-validation.ts
@@ -2,13 +2,20 @@ import { Validation } from '@/presentation/protocols'
 import { InvalidParamError } from '@/errors'
 
 export class IntegerFieldValidation implements Validation {
-  constructor (private readonly fieldName: string) {
-    this.fieldName = fieldName
-  }
+  constructor (
+    private readonly fieldName: string,
+    private readonly numberOfDigits?: number
+  ) {}
 
   validate (input: any): Error {
     if (!Number.isInteger(input[this.fieldName])) {
       return new InvalidParamError(this.fieldName)
+    }
+    if (this.numberOfDigits) {
+      const element = input[this.fieldName].toString()
+      if (element.length > this.numberOfDigits) {
+        return new InvalidParamError(this.fieldName)
+      }
     }
   }
 }
