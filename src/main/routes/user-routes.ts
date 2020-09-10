27264944e6e3c8bd4209bb0745bc8513c37271f7
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-10 15:36:07 -0300

    feat: implementada a rota GET '/users/:user_id'

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -2,8 +2,14 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeCreateUserController } from '../factories/controllers/user/create/create-user-controller-factory'
 import { makeAuthenticationUserController } from '../factories/controllers/user/authentication/authentication-user-controller-factory'
+import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { makeAuthenticationMiddleware } from '../factories/middlewares/authentication-middleware-factory'
+import { makeGetUserByIdController } from '../factories/controllers/user/get/get-by-id/get-user-by-id-controller-factory'
 
 export default (router: Router): void => {
+  const authMiddleware = adaptMiddleware(makeAuthenticationMiddleware())
+
   router.post('/users', adaptRoute(makeCreateUserController()))
+  router.get('/users/:user_id', authMiddleware, adaptRoute(makeGetUserByIdController()))
   router.post('/login', adaptRoute(makeAuthenticationUserController()))
 }
