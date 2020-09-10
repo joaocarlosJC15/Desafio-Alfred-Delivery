78b97fe605f608e9bf3ca682c83589953cd827fe
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-09 21:30:58 -0300

    refactor: renomeado diretorio

--- src/main/routes/category-routes.ts
+++ src/main/routes/category-routes.ts
@@ -3,7 +3,7 @@ import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { makeAuthenticationMiddleware } from '../factories/middlewares/authentication-middleware-factory'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeCreateCategoryController } from '../factories/controllers/category/create/create-category-controller-factory'
-import { makeGetCategoriesByUserController } from '../factories/controllers/category/get/get-categories-by-user-controller-factory'
+import { makeGetCategoriesByUserController } from '../factories/controllers/category/get/allByUser/get-categories-by-user-controller-factory'
 import { makeGetCategoryByUserController } from '../factories/controllers/category/get/oneByUser/get-category-by-user-controller-factory'
 import { makeEditCategoryController } from '../factories/controllers/category/edit/edit-category-controller-factory'
 
