6093a7ef9c334f57ee441afff24b35be76e8298c
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-10 15:21:49 -0300

    refactor: modificado parametro do factory do controller 'GetCategoriesByUserController'

--- src/main/factories/controllers/category/get/allByUser/get-categories-by-user-controller-factory.ts
+++ src/main/factories/controllers/category/get/allByUser/get-categories-by-user-controller-factory.ts
@@ -1,9 +1,9 @@
 import { Controller } from '@/presentation/protocols'
 import { GetCategoriesByUserController } from '@/presentation/controllers/category/get/get-all-by-user/get-categories-by-user-controller'
-import { makeCategoryRepository } from '@/main/factories/repositories/category/category-repository-factory'
+import { makeGetCategoriesByUserUseCase } from '@/main/factories/usecases/category/get/get-categories-by-user-usecase-factory'
 
 export const makeGetCategoriesByUserController = (): Controller => {
-  const controller = new GetCategoriesByUserController(makeCategoryRepository())
+  const controller = new GetCategoriesByUserController(makeGetCategoriesByUserUseCase())
 
   return controller
 }
