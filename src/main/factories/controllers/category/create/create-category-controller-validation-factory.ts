b9aa23db3d8d301ddce7dd94ad7ba441b41b66aa
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-09 22:36:40 -0300

    refactor: acrescentada validacao 'StringFieldValidation' para os parametros da rota POST '/categories'

--- src/main/factories/controllers/category/create/create-category-controller-validation-factory.ts
+++ src/main/factories/controllers/category/create/create-category-controller-validation-factory.ts
@@ -1,10 +1,12 @@
 import { Validation } from '@/presentation/protocols/validation'
 
-import { RequiredFieldValidation, ValidationComposite } from '@/validation'
+import { RequiredFieldValidation, ValidationComposite, StringFieldValidation } from '@/validation'
 
 export const makeCreateCategoryControllerValidation = (): ValidationComposite => {
   const validations: Validation[] = []
   validations.push(new RequiredFieldValidation('name'))
+  validations.push(new StringFieldValidation('name', 200))
+  validations.push(new StringFieldValidation('description', 200))
 
   return new ValidationComposite(validations)
 }
