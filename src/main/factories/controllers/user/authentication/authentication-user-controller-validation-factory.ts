d06ad2c1b5c771da79fb6c1321e6c3c60fa8da37
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-09 21:29:21 -0300

    refactor: adicionada validacao 'StringFieldValidation' para os parametros da rota POST '/login'

--- src/main/factories/controllers/user/authentication/authentication-user-controller-validation-factory.ts
+++ src/main/factories/controllers/user/authentication/authentication-user-controller-validation-factory.ts
@@ -1,10 +1,13 @@
 import { Validation } from '@/presentation/protocols/validation'
-import { RequiredFieldValidation, ValidationComposite } from '@/validation'
+import { RequiredFieldValidation, ValidationComposite, StringFieldValidation } from '@/validation'
 
 export const makeAuthenticationUserControllerValidation = (): ValidationComposite => {
   const validations: Validation[] = []
   for (const field of ['email', 'password']) {
     validations.push(new RequiredFieldValidation(field))
   }
+  validations.push(new StringFieldValidation('email', 200))
+  validations.push(new StringFieldValidation('password', 200))
+
   return new ValidationComposite(validations)
 }
