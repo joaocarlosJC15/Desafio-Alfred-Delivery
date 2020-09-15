8857e7a1aaf1c0dc2274639d50f843554c60c575
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-14 21:10:04 -0300

    refactor: acrescentado a dependencia 'HashGenerate' ao usecase 'EditUserUseCase' para gerar o hash da senha caso ela seja alterada

--- src/domain/usecases/user/edit/edit-user-usecase.ts
+++ src/domain/usecases/user/edit/edit-user-usecase.ts
@@ -4,33 +4,37 @@ import { GetUserByIdRepository } from '@/domain/protocols/db/user/get-user-by-id
 import { HashComparer } from '@/domain/protocols/criptography/hash/hash-comparer'
 import { UnauthorizedError, ParamInUseError } from '@/errors'
 import { GetUserByEmailRepository } from '@/domain/protocols/db/user/get-user-by-email-repository'
+import { HashGenerate } from '@/domain/protocols/criptography/hash/hash-generate'
 
 export class EditUserUseCase implements EditUser {
   constructor (
     private readonly editUserRepository: EditUserRepository,
     private readonly getUserByIdRepository: GetUserByIdRepository,
     private readonly getUserByEmailRepository: GetUserByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly hashGenerate: HashGenerate
   ) {}
 
-  async edit (user_id: number, user: EditUserModel, password?: string): Promise<boolean> {
-    if (user.email) {
-      const getUser = await this.getUserByEmailRepository.getByEmail(user.email)
+  async edit (user_id: number, userEdit: EditUserModel, password?: string): Promise<boolean> {
+    if (userEdit.email) {
+      const getUser = await this.getUserByEmailRepository.getByEmail(userEdit.email)
 
       if (getUser) {
         throw new ParamInUseError('email')
       }
     }
-    if (user.password) {
+    if (userEdit.password) {
       const getUser = await this.getUserByIdRepository.getById(user_id)
 
       const isValid = await this.hashComparer.compare(getUser.password, password)
       if (!isValid) {
         throw new UnauthorizedError()
       }
+
+      userEdit.password = await this.hashGenerate.generate(userEdit.password)
     }
 
-    await this.editUserRepository.edit(user_id, user)
+    await this.editUserRepository.edit(user_id, userEdit)
 
     return true
   }
