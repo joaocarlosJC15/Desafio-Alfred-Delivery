96589d51b7a4d0ec4d965c0f624258e69099f00d
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-14 21:24:43 -0300

    refactor: alterado o tipo do retorno dos factories dos usecases 'GetCategoriesByUserUseCase' e 'GetCategoryByUserUseCase'

--- src/main/factories/usecases/category/get/get-categories-by-user-usecase-factory.ts
+++ src/main/factories/usecases/category/get/get-categories-by-user-usecase-factory.ts
@@ -1,7 +1,8 @@
 import { GetCategoriesByUserUseCase } from '@/domain/usecases/category/get/get-all-by-user/get-categories-by-user-usecase'
 import { makeCategoryRepository } from '@/main/factories/repositories/category/category-repository-factory'
+import { GetCategoriesByUser } from '@/domain/usecases/category/get/protocols/get-categories-by-user'
 
-export const makeGetCategoriesByUserUseCase = (): GetCategoriesByUserUseCase => {
+export const makeGetCategoriesByUserUseCase = (): GetCategoriesByUser => {
   const getCategoriesByUserUseCase = new GetCategoriesByUserUseCase(makeCategoryRepository())
 
   return getCategoriesByUserUseCase
--- src/main/factories/usecases/category/get/get-category-by-user-usecase-factory.ts
+++ src/main/factories/usecases/category/get/get-category-by-user-usecase-factory.ts
@@ -1,7 +1,8 @@
 import { GetCategoryByUserUseCase } from '@/domain/usecases/category/get/get-by-user/get-category-by-user-usecase'
 import { makeCategoryRepository } from '@/main/factories/repositories/category/category-repository-factory'
+import { GetCategoryByUser } from '@/domain/usecases/category/get/protocols/get-category-by-user'
 
-export const makeGetCategoryByUserUseCase = (): GetCategoryByUserUseCase => {
+export const makeGetCategoryByUserUseCase = (): GetCategoryByUser => {
   const getCategoryByUserUseCase = new GetCategoryByUserUseCase(makeCategoryRepository())
 
   return getCategoryByUserUseCase
