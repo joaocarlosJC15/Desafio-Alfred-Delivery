2ed59fea92646b628e94349d9a0a251e71ae9bf1
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-09-16 17:19:41 -0300

    chore: implementada a migration para criacao da tabela de 'launchs'

---
+++ src/infra/db/postgresql/config/migrations/20200916163031_create_launchs_table.ts
@@ -0,0 +1,19 @@
+import * as Knex from 'knex'
+
+export async function up (knex: Knex): Promise<void> {
+  return knex.schema.createTable('launchs', (table: Knex.TableBuilder) => {
+    table.increments('id').primary()
+    table.string('description', 1000).notNullable()
+    table.string('type', 10).notNullable()
+    table.decimal('input', 10, 2).notNullable()
+    table.decimal('output', 10, 2).notNullable()
+    table.decimal('balance', 10, 2).notNullable()
+    table.specificType('date', 'timestamptz')
+    table.boolean('disabled').notNullable()
+    table.integer('category_id').references('id').inTable('categories').notNullable()
+  })
+}
+
+export async function down (knex: Knex): Promise<void> {
+  return knex.schema.dropTable('launchs')
+}
